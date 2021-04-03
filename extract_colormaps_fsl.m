f2754de9d6c9ed52d3f0eda9bf338b24549d9b2c
dmascali <danielemascali@gmail.com>
2021-04-03 19:31:41 +0200

    remove plotting, add save mat

--- extract_colormaps_fsl.m
+++ extract_colormaps_fsl.m
@@ -1,32 +1,7 @@
 function extract_colormaps_fsl(varargin)
-
-
-% %--------------VARARGIN----------------------------------------------------
-% params  =  {'extract'};
-% defParms = {cellfun(@(x) ['img',x],layerStrings,'UniformOutput',0)', ...
-%             cellfun(@(x) [min(x(:)) max(x(:))],img,'UniformOutput',0),... % use min and max in each image as limits
-%             cell(1,nLayers),...
-%             colorbarDefaultList(1:nLayers),...
-%             num2cell(ones(1,nLayers)),...
-%             'best', [0 0 0 0], [0 0],  [2 6],   'ax', '300',cell(1,nLayers), 'auto', [0.2 0.2], 'k'};
-% legalValues{1} = [];
-% legalValues{2} = [];
-% legalValues{3} = [];
-% legalValues{4} = [];
-% legalValues{5} = [];
-% legalValues{6} = {'best','south','east'};
-% legalValues{7} = [];
-% legalValues{8} = [];
-% legalValues{9} = [];
-% legalValues{10} = {'ax','sag','cor'};
-% legalValues{11} =[];
-% legalValues{12} =[];
-% legalValues{13} =[];
-% legalValues{14} =[];
-% legalValues{15} = {'k','black','w','white'};
-% [labels,limits,minClusterSize,colormaps,alpha,cbLocation,margins,innerMargins,mount,view,resolution,zScore,slices,skip,colorMode] = ParseVarargin(params,defParms,legalValues,varargin,1);
-% %--------------------------------------------------------------------------
-
+% This function needs to be run just once to create "cmaps.mat" that stores
+% various colormaps, including those from FSL, Brain Colours and also those
+% from matlab (yet, created at high resolution: 256 levels). 
 
 %------- load FSL colormaps (include braincolours)-------------------------
 location = '/usr/local/fsl/fslpython/envs/fslpython/lib/python3.7/site-packages/fsleyes/assets/colourmaps';
@@ -49,7 +24,6 @@ for l = 1:length(order)
     cmap(l).map = map;
 end
 %--------------------------------------------------------------------------
-
 
 %------- load builtin matlab color maps------------------------------------
 % add also matlab colormap
@@ -74,47 +48,5 @@ end
 % total number of maps
 nMaps = length(cmap);
 
-% prepare figure and find axe locations
-mount = [27,4];
-[hFig,axesPos,~,figPos] = figure_grid(mount,[155,25],[0.01 0.20 0.01 0.01],[0.27 0.005],0,'none',0);
-
-
-countMap = 1; %counter for maps
-count = 0;  %counter for axes
-for col = 1:mount(2)
-    for row = 1:mount(1)
-        % exit the loop if no more maps
-        if countMap > nMaps
-            break
-        end
-       
-        %---print section title--------------------------------------------
-        count = count +1;
-        if count == 1 || (skip == 0 && ~(strcmp(cmap(countMap).source,cmap(countMap-1).source))) %any(count == StartSections)
-            %just 
-            ax = axes('Position',axesPos{row,col});
-            text(0,0.5,cmap(countMap).source,'Unit','Normalized','verticalalignment','middle','interpreter','none','fontsize',12,'fontweight','bold');
-            set(gca,'Visible','off');
-            skip = 1;
-            continue
-        end
-        skip = 0;
-        %------------------------------------------------------------------
-        
-        ax = axes('Position',axesPos{row,col});
-        x = 1:1:length(cmap(countMap).map);
-        imagesc(x); colormap(ax,cmap(countMap).map);
-        text(1.02,0.5,cmap(countMap).name,'Unit','Normalized','verticalalignment','middle','interpreter','none','fontsize',11,'fontweight','normal');
-        set(gca,'Xtick',[]);set(gca,'Ytick',[]);
-        countMap = countMap +1;
-    end
-end
-
-set(hFig,'Name','Colormap List','NumberTitle','off')
-
-set(gcf, 'InvertHardcopy', 'off','PaperPositionMode','auto');
-%try to force again position. It works!
-set(gcf,'Position',figPos);
-print('colormapList.png','-dpng','-r300')
-
+save('cmaps','cmap','nMaps');
 end
