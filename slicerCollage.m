f03627d2653bb129cd1b3bdd4ad459e4fc5638a5
dmascali <danielemascali@gmail.com>
2021-04-22 22:09:45 +0200

    use imwrite to print, add error checking for wrong image size

--- slicerCollage.m
+++ slicerCollage.m
@@ -1,10 +1,9 @@
 function slicerCollage(list,varargin)
 
-
-
 if nargin == 0
-    %search slicer image in the current folder
-    list = dir(['slicer_*.png']);
+    %if no input is provided. Try to guess every parameter. Look for slicer
+    %images in the current folder. 
+    list = dir('slicer_*.png');
     if isempty(list)
         disp('No slicer png found.');
     end
@@ -13,54 +12,56 @@ if nargin == 0
     %determin the dimenion to concatenate.
     %todo check dim compatibility
     img = imread(list(1).name);
-    s = size(img); [~,dim] = min(s(1:2));
+    s1 = size(img); [~,dim] = min(s1(1:2));
     output = 'slicerCollage';
+    showFigure = 1;
 else
     %--------------VARARGIN----------------------------------------------------
-    params  =  {'order','dim','output'};
-    defParms = {     [],   [],      'slicerCollage'};
+    params  =  {'order','dim',       'output','showFigure'};
+    defParms = {     [],   [],'slicerCollage',           1};
     legalValues{1} = [];
     legalValues{2} = [1,2];
     legalValues{3} = [];
-    [order,dim,output] = ParseVarargin(params,defParms,legalValues,varargin,1);
+    legalValues{4} = [0,1];
+    [order,dim,output,showFigure] = ParseVarargin(params,defParms,legalValues,varargin,1);
     %--------------------------------------------------------------------------
 end
    
 if isempty(list)
-    list = dir(['slicer_*.png']);
+    list = dir('slicer_*.png');
     if isempty(list)
         disp('No slicer png found.');
     end
 end
 nImg = length(list);
-   
-if isempty(dim)
-    img = imread(list(1).name);
-    s = size(img); [~,dim] = min(s(1:2));    
-end
-  
+
 if isempty(order)
     order = 1:nImg;
 end
-
-    
+   
+if isempty(dim)
+    img = imread(list(order(1)).name);
+    s1 = size(img); [~,dim] = min(s1(1:2));    
+end
+  
 IMG = [];
 for l = order
    img = imread(list(l).name); 
+   if l > 1 %check size consistency
+      if any(size(img)~= s1)
+          error('Images must have equal size.');
+      end
+   end
    IMG = cat(dim,IMG,img);
 end
-figure; 
-imshow(IMG,'border','tight');
-set(gca,'units','pixels') % set the axes units to pixels
-x = get(gca,'position'); % get the position of the axes
-set(gcf,'units','pixels') % set the figure units to pixels
-y = get(gcf,'position'); % get the figure position
-set(gcf,'position',[y(1) y(2) x(3) x(4)])% set the position of the figure to the length and width of the axes
-set(gca,'units','normalized','position',[0 0 1 1]) % set the axes units to pixels
+imwrite(IMG,[output,'.png']);
 
-
-print([output,'.png'],'-dpng',['-r300'])
-
+if showFigure
+    figure('Name','SlicerCollage','MenuBar', 'None');
+    warning off
+    imshow(IMG,'border','tight');
+    warning on
+end
     
 return
 end
