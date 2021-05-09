00ec8a22407020f9757d051f1290d59c3a7ecdec
dmascali <danielemascali@gmail.com>
2021-05-09 22:59:50 +0200

    now all variables are handled in varargin

--- slicerCollage.m
+++ slicerCollage.m
@@ -1,4 +1,4 @@
-function slicerCollage(list,varargin)
+function slicerCollage(varargin)
 %SLICERCOLLAGE Combine multiple slicer's images into a single image.
 %  SLICERCOLLAGE() searches into the current folder for any slicer's image
 %   and concatenate them along the longest dimension. To be concatenated
@@ -10,8 +10,17 @@ function slicerCollage(list,varargin)
 %   (each parameter must be followed by its value ie, 'param1',value1, 
 %   'param2',value2):  
 %
-%   COLORMAP('default') sets the current figure's colormap to
-%   the root's default, whose setting is PARULA.
+%    dim           - Select the dimension along which to concatenate the
+%                    images. Default: the longest dimension. 
+%    order         - Vector of integers indicating the concatenation order.
+%                    Default: 1:N, where N is the number of images. 
+%    output        - Output name. Default: 'slicerCollage'. 
+%    folder        - The path (relative or absolute) of the folder 
+%                    containing slicer images. Default: './' (i.e., the 
+%                    folder).
+%    wildcard      - Char expression for matching slicer images (e.g.,:
+%                    'slicer_BOLD*.png'). Default: 'slicer_*.png'. 
+%    show          - Show combined image. Default: true. 
 %
 %   See also SLICER, COLORMAPS
 
@@ -24,44 +33,32 @@ function slicerCollage(list,varargin)
 % change showfigure to show
 % concatenate opt variable if mat files are present
 
-if nargin == 0
-    %if no input is provided. Try to guess every parameter. Look for slicer
-    %images in the current folder. 
-    list = dir('slicer_*.png');
-    if isempty(list)
-        disp('No slicer images found.');
-        return
-    end
-    nImg = length(list);
-    order = 1:nImg;
-    %determin the dimenion along which to concatenate images
-    %todo check dim compatibility
-    img = imread(list(1).name);
-    s1 = size(img); [~,dim] = min(s1(1:2));
-    output = 'slicerCollage';
-    showFigure = 1;
-else
-    %--------------VARARGIN----------------------------------------------------
-    params  =  {'order','dim',       'output','showFigure'};
-    defParms = {     [],   [],'slicerCollage',           1};
-    legalValues{1} = [];
-    legalValues{2} = [1,2];
-    legalValues{3} = [];
-    legalValues{4} = [0,1];
-    [order,dim,output,showFigure] = ParseVarargin(params,defParms,legalValues,varargin,1);
-    %--------------------------------------------------------------------------
-end
-   
+%--------------VARARGIN----------------------------------------------------
+params  =  {'order','dim',       'output','showFigure','folder','wildcard'};
+defParms = {     [],   [],'slicerCollage',           1,    './','slicer_*.png'};
+legalValues{1} = [];
+legalValues{2} = [1,2];
+legalValues{3} = [];
+legalValues{4} = [0,1];
+legalValues{5} = [];
+legalValues{6} = [];
+[order,dim,output,showFigure,folder,...,
+    wildcard] = ParseVarargin(params,defParms,legalValues,varargin,1);
+%--------------------------------------------------------------------------
+ 
 %get function name
 funcName = mfilename;
 fprintf('%s - welcome\n',funcName);
 
+%get list of slicer images
+if folder(end) ~= '/'; folder(end+1) = '/'; end
+pattern = [folder,wildcard];
+fprintf('%s - looking for images using the pattern:\n',funcName);
+fprintf('%s \n',pattern);
+list = dir(pattern);
 if isempty(list)
-    list = dir('slicer_*.png');
-    if isempty(list)
-        disp('No slicer png found.');
-        return
-    end
+    disp('No slicer png found.');
+    return
 end
 nImg = length(list);
 
@@ -78,6 +75,7 @@ fprintf('%s - dime  = %d\n',funcName,dim);
 fprintf(['%s - order = ',repmat('%d ', 1, length(order)),'\n'],funcName,order);
 fprintf('%s - concatenating images:\n',funcName);
 
+% determine the minimum size across images.
 count = 1;
 img = cell(length(order),1);
 sizeImg = nan(length(order),2);
@@ -104,27 +102,6 @@ for l = order
    count = count +1;
 end
 imwrite(IMG,[output,'.png']);
-
-% IMG = [];
-% count = 1;
-% for l = order
-%    img = imread(list(l).name); 
-%    fprintf('-> %d(%d) %s\n',count,l,list(l).name);
-%    if l > 1 %check size consistency
-%       if any(size(img)~= s1)
-%           %let's see if it is just one voxel. Sometimes this happens.
-%           if sum((size(img) - s1)) <=2
-%               img = img(1:s1(1),1:s1(2),:);
-%               fprintf('One voxel mismatch found, the image has been cropped.');
-%           else
-%             error('Images must have equal size.');
-%           end
-%       end
-%    end
-%    IMG = cat(dim,IMG,img);
-%    count = count +1;
-% end
-% imwrite(IMG,[output,'.png']);
 
 if showFigure
     figure('Name','SlicerCollage','MenuBar', 'None');
