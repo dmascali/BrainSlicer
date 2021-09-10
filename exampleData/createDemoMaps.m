822c3f2af74d016994718cf5df156a6f17fcd913
dmascali <danielemascali@gmail.com>
2021-09-10 16:05:16 +0200

    init

--- exampleData/createDemoMaps.m
+++ exampleData/createDemoMaps.m
@@ -1,19 +1,31 @@
-function createDemoMaps(atlas,mu,sigma,name)
+function createDemoMaps(atlas,nu)
 
 hdr = spm_vol(atlas);
 img = spm_read_vols(hdr);
 nROIs =length(unique(img(:)))-1;
 
-y = abs(mu + sigma*(randn(1,nROIs)));
+t = abs(trnd(nu,1,nROIs));
+thr = tinv(1-0.05,nu);
+p = 1-tcdf(t,nu-1);
 
+T = img;
+P = img;
 for l = 1:nROIs
-    img(img == l) = y(l);
+    T(img == l) = t(l);
+    P(img == l) = p(l);
 end
 
 HDR = hdr;
-HDR.fname = [name,'.nii'];
-HDR.private.dat.fname = [name,'.nii'];
-spm_write_vol(HDR,img);
+HDR.dt = [16 0];
+HDR.fname = ['t-map_',num2str(thr,4),'.nii'];
+HDR.private.dat.fname = ['t-map_',num2str(thr,4),'.nii'];
+spm_write_vol(HDR,T);
+
+HDR = hdr;
+HDR.dt = [16 0];
+HDR.fname = ['p-map.nii'];
+HDR.private.dat.fname = ['p-map.nii'];
+spm_write_vol(HDR,P);
 
 
 return
