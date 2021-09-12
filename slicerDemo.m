99ef622ae3bdc9cd00865b556cfbd28b995e9cc5
dmascali <danielemascali@gmail.com>
2021-09-12 12:49:43 +0200

    working on example 3

--- slicerDemo.m
+++ slicerDemo.m
@@ -9,6 +9,10 @@ data_folder = [fileparts(which('slicer')),'/exampleData/'];
 close all
 %%
 %  Now, we are ready to start!
+%
+% TODO: 
+% negative values in t-map
+
 
 %% Example 1: standard + one-side t-map
 % Let's start with a very simple figure: just one layer occupied by a standard brain.
@@ -31,7 +35,7 @@ slicer({2},'output','example_1')
 % That's cool, but it's not very helpfull if we don't overlay something 
 % interesting. So, let's add a layer with a T-map. Default values won't probably
 % work this time, so we manually specify the t-map limits and the t-map minimum 
-% cluster size.
+% cluster size:
 slicer({2,[data_folder,'spmT_0001.nii']},...
     'limits',{[],[3.5 6]},... % when a layer's limit is empty, limits will be adjusted automatically
     'minClusterSize',{0,50},....
@@ -40,7 +44,7 @@ slicer({2,[data_folder,'spmT_0001.nii']},...
 % It's already a good starting point, but we can make the figure a little
 % fancier. For instance, we don't need the colorbar for the standard image, so
 % we can modify the default behaviour using the property "labels". We can also move
-% the Tmap's colorbar to the east side and add a proper figure title.
+% the Tmap's colorbar to the east side and add a proper figure title:
 slicer({2,[data_folder,'spmT_0001.nii']},...
     'limits',{[],[3.5 6]},...
     'minClusterSize',{0,50},...
@@ -50,7 +54,7 @@ slicer({2,[data_folder,'spmT_0001.nii']},...
     'titleLocation','center',...
     'output','example_1')
 %%
-% Wow! You got a prety cool figure, and it's ready for publication!
+% Wow! We got a prety cool figure, and it's ready for publication!
 % However, depending on your taste you might want to experiment a bit with
 % other options, such as the number of slices:
 slicer({2,[data_folder,'spmT_0001.nii']},...
@@ -62,7 +66,8 @@ slicer({2,[data_folder,'spmT_0001.nii']},...
     'mount', [1 8],... % print one row with 8 slices equally spaced
     'output','example_1_fancier')
 
-%% Or the background color:
+%% 
+%Or the background color:
 slicer({2,[data_folder,'spmT_0001.nii']},...
     'limits',{[],[3.5 6]},...
     'minClusterSize',{0,50},...
@@ -86,37 +91,70 @@ slicer({2,[data_folder,'spmT_0001.nii'], [data_folder,'spmT_0001.nii']},...
     'title','Just a Random two-side T-map',...
     'mount', [1 8],... % print one row with 8 slices equally spaced
     'output','example_2')
-%% Note that we put the negative layer before the positive layer, so to get 
+%%
+% Note that we put the negative layer before the positive layer, so to get 
 % the negative colorbar below the positive one.
 % We are close to the solution, yet you might have noticed that the default
-% colormaps looks odd. It's time to tweak the property 'colormaps'. You can
-% get the full list of available colormaps with associated labels and codes
-% by running the command:
+% colormaps looks odd (blue for positive values and red for negative values)
+% It's time to tweak the property 'colormaps'. You can get the full list of
+% available colormaps (with associated labels and codes) by running the command:
 colormaps
-%% To get a more common
 
-%%
-slicer({2,[data_folder,'t-map_1.66.nii'],[data_folder,'atlas_edges_120.nii']},...
-    'limits',{[],[1.7 4],[0 1]},...
-    'minClusterSize',{0,0,0},...
-    'labels',{[],'T-value',[]},... % when a layer's label is empty no colorbar will be printed.
+%% 
+% By default, slicer uses 1,2,3 (i.e., grey, red-yellow and blue-lightblue)
+% as the colormaps for the first, seond and third layer, respectively. 
+% To invert the color of the second and third layer we have to manually
+% specify the colormaps using the property 'colormaps':
+slicer({2,[data_folder,'spmT_0001.nii'], [data_folder,'spmT_0001.nii']},...
+    'limits',{[],[-6 -3.5], [3.5 6]},...
+    'minClusterSize',{0,50,0},...
+    'labels',{[],'T-value','T-value'},... % when a layer's label is empty no colorbar will be printed.
+    'cbLocation','east',... % colorbar location can be south or east
+    'title','Just a Random two-side T-map',...
+    'mount', [1 8],... % print one row with 8 slices equally spaced
+    'colormaps',{1,3,2},...
+    'output','example_2')
+ 
+%% Example 3: slicerCollage
+% In this example, we will show you how to combine different figures into a
+% single one using slicerCollage. We will start from scratch combining a
+% two-side t-map with a p-map. This time we will pretend our results were
+% generated using an atlas (AALv2 - 120 regions). Let's start from the two-side 
+% t-map figure:
+slicer({2,[data_folder,'t-map_1.66.nii'],[data_folder,'t-map_1.66.nii'],[data_folder,'atlas_edges_120.nii']},...
+    'limits',{[],[-4 -1.66],[1.66 4],[0 1]},...
+    'minClusterSize',{0,0,0,0},...
+    'labels',{[],'T-value','T-value',[]},... % when a layer's label is empty no colorbar will be printed.
     'cbLocation','east',... % colorbar location can be south or east
     'title','Just a Random T-map',...
-    'titleLocation','center',...
     'mount', [1 8],... % print one row with 8 slices equally spaced
-    'colormaps',{1,2,64},...
-    'output','standard_2mm_plus_Tmap_fancier')
+    'colormaps',{1,3,2,64},...
+    'output','example_3_01')
+%%
+% Compared to example 2, we have added a fouth layer containing the edges
+% of the atlas. Since the edge volume is a binary mask, we set the limits
+% to [0 1] and selected one of the colormaps (the 64th) which saturated
+% to a black colour.
+% Now, we can work on the second figure: the p-map. P-maps are difficult to
+% display since they range from 0 to 1, with smaller values being more
+% significant than greater values. To overcome this issue, we can set the 
+% property 'p-map' to 1. This property will force slicer to display 1-p, so
+% that we can set the limits to something like [0.95 1]:
 
 slicer({2,[data_folder,'p-map.nii'],[data_folder,'atlas_edges_120.nii']},...
     'limits',{[],[0.95 1],[0 1]},...
     'minClusterSize',{0,0,0},...
-    'labels',{[],'P-value',[]},... % when a layer's label is empty no colorbar will be printed.
+    'labels',{[],'1 - P-value',[]},... % when a layer's label is empty no colorbar will be printed.
     'cbLocation','east',... % colorbar location can be south or east
     'title','Just a Random P-map',...
     'titleLocation','center',...
     'mount', [1 8],... % print one row with 8 slices equally spaced
     'colormaps',{1,3,64},...
-    'p-map',{0,1,0},...
-    'output','standard_2mm_plus_Pmap_fancier')
+    'p-map',{0,1,0},... % specify that the 2nd layer is a p-map (slicer will show 1-p)
+    'output','example_3_02')
+%%
+% We have produced two figures (ecample_3_01.png and example_3_02.png) one
+% for the t map and one for the p map. To concatenate them in a single
+% figure we can call slicerCollage
 
 slicerCollage
